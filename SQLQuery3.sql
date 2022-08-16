b53c440b7efa6951ebc2c55c3dd4923ae8443b78
anhdz1102 <96427913+thean2001123@users.noreply.github.com>
2022-08-16 11:01:53 +0700

    bai tap

---
+++ SQLQuery3.sql
@@ -0,0 +1,15 @@
+create table Client(
+Id int primary key identity(1,1),
+Name varchar(255) not null unique 
+);
+create table Address(
+Id int primary key identity(1,1),
+Name varchar(255) not null unique,
+ClientId int not null foreign key references Client(Id) 
+);
+create table SubscriberNumber(
+Id int primary key identity(1,1),
+Name varchar(255) not null unique,
+RegistrationDate date,
+SubscriberId int not null foreign key references Client(Id) 
+);
