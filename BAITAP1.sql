83733d730094e7db958fa6a7636ea9c5e4becff8
anhdz1102 <96427913+thean2001123@users.noreply.github.com>
2022-08-27 19:48:25 +0700

    bai tap

--- BAITAP1.sql
+++ BAITAP1.sql
@@ -23,47 +23,47 @@ values (N'Máy tính T450', N'Máy nhập cũ', N'Chiếc', 1000, 10, 123),
  (N'Điện thoại Nokia5670', N'Điện thoại đang hot', N'Chiếc', 200, 200, 123),
  (N'Máy in Samsung 450', N'Máy in đang loại binh', N'Chiếc', 100, 10, 123);
 
- --4a 
+ --4a Hiển thị tất cả các hãng sản xuất
  select tenhang from hang
- --4b
+ --4b Hiển thị tất cả sản phẩm
  select tensanpham from sanpham
 
 
- -- 5a
+ -- 5a Liệt kê danh sách hãng theo thứ thự ngược với alphabet của tên
  insert into hang ( mahang, tenhang, diachi, dienthoai) values (124, N'Macbook', N'USA', N'032546');
 
  select tenhang from hang order by tenhang desc
 
- -- 5b
+ -- 5b Liệt kê danh sách sản phẩm của cửa hàng theo thứ thự giá giảm dần
  select tensanpham from sanpham order by gia asc
 
- -- 5c 
+ -- 5c Hiển thị thông tin của hãng Asus
  select * from hang where tenhang like N'Asus'
 
- --5d 
+ --5d Liệt kê danh sách sản phẩm còn ít hơn 11 chiếc trong kho 
  select * from sanpham where soluong < 11
 
  
 insert into sanpham( tensanpham, mota, donvi, gia, soluong, mahang) 
 values (N'Macbook M1', N'Máy nhập cũ', N'Chiếc', 1000, 10, 124);
- --5e
+ --5e Liệt kê danh sách sản phẩm của hãng Asus
  select * from sanpham as s inner join hang as h on s.mahang = h.mahang where h.tenhang like N'Asus'
 
 
 
- -- 6a
+ -- 6a Số hãng sản phẩm mà cửa hàng có
  select count(*) as 'soluong' from hang 
 
-  -- 6b
+  -- 6b Số mặt hàng mà cửa hàng bán
 
    select count(*) as 'soluong' from sanpham
    
-   -- 6c 
+   -- 6c Tổng số loại sản phẩm của mỗi hãng có trong cửa hàng
  select h.tenhang, count(s.tensanpham) as 'soluong' from sanpham as s inner join hang as h on s.mahang = h.mahang group by h.tenhang
 
-    -- 6d
+    -- 6d Tổng số đầu sản phẩm của toàn cửa hàng
  select h.tenhang, sum(s.soluong) as 'soluong' from sanpham as s inner join hang as h on s.mahang = h.mahang group by h.tenhang
 
- -- 7a
+ -- 7a Viết câu lệnh để thay đổi trường giá tiền của từng mặt hàng là dương(>0)
  create index index_tenhang on hang(tenhang)
   create index index_mota on sanpham(mota)
